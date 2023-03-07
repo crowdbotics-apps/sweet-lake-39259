67f3eb70fd1336ca9915fa821217dec1a0bff53a
Crowdbotics <team@crowdbotics.com>
2023-03-07 09:05:33 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "sweet-lake-39259",
+  url: "https://sweet-lake-39259.botics.co",
+  api: "https://sweet-lake-39259.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
